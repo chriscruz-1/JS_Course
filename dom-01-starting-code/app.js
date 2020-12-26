ab51980a20ccbf08139f4a0fc6bb84e385ed37e9
Chris C <cruzc007@gmail.com>
2020-12-25 16:38:48 -0800

    Now reviewing querying with the DOM

---
+++ dom-01-starting-code/app.js
@@ -0,0 +1,4 @@
+const h1 = document.getElementById('main-title');
+
+h1.style.color = "white";
+h1.style.backgroundColor = "black";
---
+++ dom-assignment-start/assignment.js
@@ -0,0 +1,12 @@
+const task1 = document.getElementById("task-1");
+const task1A = document.querySelector("li");
+
+task1A.style.backgroundColor = "red";
+
+const task2 = document.querySelector("title");
+
+task2.text = "Solved!";
+
+const task3 = document.querySelector("h1");
+
+task3.textContent = "Solved!";
